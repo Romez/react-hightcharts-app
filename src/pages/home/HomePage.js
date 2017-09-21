da372e50d3386c03e1495c31fc7b7386c39dfcbc
roman <TurnPikeLane.981>
2017-09-21 17:50:15 +0300

    home page рефакторинг

--- src/pages/home/HomePage.js
+++ src/pages/home/HomePage.js
@@ -12,10 +12,13 @@ import './styles.less';
 class HomePage extends React.Component {
     static path = '/';
 
+    /** Данные из файла нужные для графика и фильтров */
+    static preparedData = [];
+
     constructor(props) {
         super(props);
         HighchartsExporting(ReactHighcharts.Highcharts);
-        bindAll(this, ['onChange', 'handleData']);
+        bindAll(this, ['onChange', 'handleData', 'prepareData']);
         this.state = {
             data: [],
             System: '',
@@ -23,38 +26,53 @@ class HomePage extends React.Component {
         };
     }
 
+    prepareData() {
+        this.preparedData = bugs
+            .map((item) => {
+                if (item['Дата создания'] && item['System'] && item['Критичность']) {
+                    return {
+                        time: Date.parse(item['Дата создания']),
+                        System: item['System'],
+                        Критичность: item['Критичность']
+                    };
+                }
+            })
+            .sort((a, b) => a.time - b.time);
+        this.handleData(this.preparedData);
+    }
+
+    /**
+     * дефолтное значение если нужна будет кнопка сбросить
+     * @param bugList array
+     */
+    handleData(bugList = this.preparedData) {
+        const data = [];
+        bugList.map(
+            (item) => {
+                const time = item.time;
+                if (data[time]) {
+                    data[time][1]++;
+                } else {
+                    data[time] = [time, 1];
+                }
+            }
+        );
+        this.setState({data});
+    }
+
     componentWillMount() {
-        this.handleData();
+        this.prepareData();
     }
 
     onChange(name, value) {
         this.setState({[name]: value}, () => {
-            const bugList = bugs.filter(bug => {
+            const bugList = this.preparedData.filter((bug) => {
                 return (bug['Критичность'].toLowerCase().indexOf(this.state['Критичность'].toLowerCase()) >= 0) &&
                     (bug['System'].toLowerCase().indexOf(this.state['System'].toLowerCase()) >= 0);
             });
 
             this.handleData(bugList);
         });
-    }
-
-    handleData(bugList = bugs) {
-        const data = [];
-        bugList
-            .map(item => Date.parse(item['Дата создания']))
-            .sort((a, b) => a - b)
-            .map(utcTime => {
-                if (data[utcTime]) {
-                    data[utcTime].y++;
-                } else {
-                    data[utcTime] = {
-                        x: utcTime,
-                        y: 1
-                    };
-                }
-            });
-
-        this.setState({data});
     }
 
     render() {
